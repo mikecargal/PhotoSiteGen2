ae2201368d5dea2d7d9228f18a4050f7eabe4367
Mike Cargal <mike@cargal.net>
2024-11-10 14:58:16 -0500

    Initial Commit

---
+++ PhotoSiteGen2/PhotoSiteGen2App.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoSiteGen2App.swift
+//  PhotoSiteGen2
+//
+//  Created by Mike Cargal on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotoSiteGen2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
