600581299fc945592348ea0eea9dcb469ebed813
Mike Cargal <mike@cargal.net>
2024-11-18 15:42:53 -0500

    Brought over generation related code from original project (not tied in yet)

---
+++ PhotoSiteGen2/Views/GallerySourceDirectorySelector.swift
@@ -0,0 +1,18 @@
+//
+//  GallerySourceDirectorySelector.swift
+//  PhotoSiteGen2
+//
+//  Created by Mike Cargal on 11/18/24.
+//
+
+import SwiftUI
+
+struct GallerySourceDirectorySelector: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GallerySourceDirectorySelector()
+}
---
+++ PhotoSiteGen2/Views/TitleImageEditView.swift
@@ -0,0 +1,18 @@
+//
+//  TitleImageEditView.swift
+//  PhotoSiteGen2
+//
+//  Created by Mike Cargal on 11/18/24.
+//
+
+import SwiftUI
+
+struct TitleImageEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TitleImageEditView()
+}
