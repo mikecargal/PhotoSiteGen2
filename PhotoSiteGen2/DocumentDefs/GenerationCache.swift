849c359cb6552c3869b3cdeecbaddf10047f0c1b
Mike Cargal <mike@cargal.net>
2025-01-03 16:23:40 -0500

    cleanup

--- PhotoSiteGen2/DocumentDefs/GenerationCache.swift
+++ PhotoSiteGen2/DocumentDefs/GenerationCache.swift
@@ -8,9 +8,24 @@
 import Foundation
 
 struct GenerationCache: Codable {
-    let galleryPhotosCache: [UUID: [URL:Photo]]
+    let galleryPhotosCache: [UUID: [URL: Photo]]
 
-    func photos(for galleryId: UUID) -> [URL:Photo]? {
+    func photos(for galleryId: UUID) -> [URL: Photo]? {
         galleryPhotosCache[galleryId]
+    }
+
+    func getCameras() -> [String: URL] {
+        var cameras = [String: URL]()
+        galleryPhotosCache.forEach { (uuid, dict) in
+            dict.forEach { (url, photo) in
+                if let camera = photo.metadata.camera {
+                    if cameras[camera] == nil {
+                        cameras[camera] = url
+                    }
+                }
+            }
+        }
+        debugPrint("Cameras: \(cameras)")
+        return cameras
     }
 }
